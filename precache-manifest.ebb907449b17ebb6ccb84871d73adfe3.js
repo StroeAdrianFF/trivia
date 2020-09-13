5e663dfd95dcd46d74e44c1345e24e4edc7ef027
Adrian <contact@stroe-adrian.ro>
2020-09-13 13:01:14 +0300

    Updates

---
+++ precache-manifest.ebb907449b17ebb6ccb84871d73adfe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc162445ebd34d67061f3e6ebef74bc",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "c2332ac89fd9f788757b",
+    "url": "/trivia/static/css/main.80c02c39.chunk.css"
+  },
+  {
+    "revision": "866eb72ccd55c47c907c",
+    "url": "/trivia/static/js/2.00301f0e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trivia/static/js/2.00301f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2332ac89fd9f788757b",
+    "url": "/trivia/static/js/main.7cc24872.chunk.js"
+  },
+  {
+    "revision": "194729bae84278f9686e",
+    "url": "/trivia/static/js/runtime-main.82c86a55.js"
+  }
+]);
